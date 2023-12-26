c1939755c6f87566361bdf47b9c7e2d5bc19d4d5
James Zhang <jjz002@ucsd.edu>
2023-12-25 18:24:28 -0800

    Fix 404 not found error

---
+++ src/components/CalendarDemo.tsx
@@ -0,0 +1,22 @@
+// "use client";
+
+import React, { useEffect } from "react";
+
+import { Calendar } from "@/components/ui/calendar";
+
+export function CalendarDemo() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+  useEffect(() => {
+    console.log("DATE: " + date);
+  }, [date]);
+
+  return (
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
+  );
+}
