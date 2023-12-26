652cf0e391b7156446d6d5ee6def999f8db12edd
James Zhang <jjz002@ucsd.edu>
2023-12-25 19:20:15 -0800

    Create working stopwatch component

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,25 +1,57 @@
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import styles from "../styles/Stopwatch.module.css";
 
 export default function Stopwatch() {
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formatTime = (timeInSeconds: number): string => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    const formattedHours = String(hours).padStart(2, "0");
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
   return (
-    <div className="stopwatch">
-      <span className="stopwatch" id="display">
-        00:00:00
-      </span>
-      <div className="controls">
-        <button className="play-button">
-          <Image src="/img/play.svg" width={100} height={100} alt="play" />
+    <div className={styles.stopwatch}>
+      <span className={styles["time-display"]}>{formatTime(time)}</span>
+      <div className={styles.controls}>
+        <button className={styles["start-stop"]} onClick={handleStartStop}>
+          {isRunning ? (
+            <Image src="/img/pause.svg" width={50} height={50} alt="pause" />
+          ) : (
+            <Image src="/img/play.svg" width={50} height={50} alt="play" />
+          )}
         </button>
-        <button className="pause-button">
-          <Image src="/img/pause.svg" width={100} height={100} alt="pause" />
-        </button>
-        <button className="restart-button">
-          <Image
-            src="/img/restart.svg"
-            width={100}
-            height={100}
-            alt="restart"
-          />
+        <button className={styles.reset} onClick={handleReset}>
+          <Image src="/img/restart.svg" width={50} height={50} alt="restart" />
         </button>
       </div>
     </div>
