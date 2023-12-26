74028b0420e53fd76a208ceb65d669cd411a2575
James Zhang <jjz002@ucsd.edu>
2023-12-25 18:44:46 -0800

    Add play icon image

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import Image from "next/image";
 
 export default function Stopwatch() {
-  return <div>Stopwatch</div>;
+  return (
+    <div className="stopwatch">
+      <span className="stopwatch" id="display">
+        00:00:00
+      </span>
+      <div className="controls">
+        <Image src="/img/play.svg" width={100} height={100} alt="play" />
+      </div>
+    </div>
+  );
 }
