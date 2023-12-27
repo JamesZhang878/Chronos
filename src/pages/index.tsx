d5e8961c032a732129812765e7a146a2e9f570ba
James Zhang <jjz002@ucsd.edu>
2023-12-26 21:22:19 -0800

    WIP: adding tabs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
 import { Inter } from "next/font/google";
-import Stopwatch from "@/components/Stopwatch";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import styles from "../styles/Index.module.css";
+import Timing from "./timing";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Stopwatch />
+      <Tabs className={styles["TabsRoot"]} defaultValue="timing">
+        <TabsList className={styles["TabsList"]}>
+          <TabsTrigger className={styles["TabsTrigger"]} value="account">
+            <Image
+              className={styles["TabsIcon"]}
+              src="/img/clock.svg"
+              width={35}
+              height={35}
+              alt="clock"
+            />
+          </TabsTrigger>
+          <TabsTrigger className={styles["TabsTrigger"]} value="password">
+            Password
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent className={styles["TabsContent"]} value="account">
+          <Timing />
+        </TabsContent>
+        <TabsContent className={styles["TabsContent"]} value="password">
+          Change your password here.
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
---
+++ src/pages/timing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Stopwatch from "@/components/Stopwatch";
+import styles from "../styles/Timing.module.css";
+
+const Timing = () => {
+  return <Stopwatch />;
+};
+
+export default Timing;
